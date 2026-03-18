1950cb51938818ca9d8361c4c01ce8ec09a63780
JesusRC22 <jesusriosc22@gmail.com>
2026-03-18 14:21:03 +0100

    Prueba de creación del index.html con el render.

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+import { writeFile } from "fs/promises";
 import { renderHtml,renderHtmlDetalle } from "./render.js";
 import { loadProducts } from "./products.js";
 
 var products = await loadProducts(100);
-//const html = renderHtml(products);
+const html = renderHtml(products);
 const htmlDetalle = renderHtmlDetalle(products[52]);
 
-console.log(htmlDetalle);
+await writeFile('index.html', html);
+//console.log(htmlDetalle);
